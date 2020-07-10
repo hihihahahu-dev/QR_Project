8f1eba4abc1549e65a3d0e9f794c6e72f1a9adaa
mingyig3 <35508418+mingyig3@users.noreply.github.com>
2020-07-10 02:38:52 -0500

    Commit

--- app/src/main/java/com/example/qrproject/MainActivity.java
+++ app/src/main/java/com/example/qrproject/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.qrproject;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,26 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Set Internet Request
-        final TextView mTextView = (TextView) findViewById(R.id.testing);
         final ImageView mImageView = (ImageView) findViewById(R.id.imageView1);
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100";
-        final ImageRequest imageRequest = new ImageRequest(url,
-                new Response.Listener<Bitmap>() {
-                    @Override
-                    public void onResponse(Bitmap response) {
-                        // Display the first 500 characters of the response string.
-                        mImageView.setImageBitmap(response);
-                    }
-                }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-                mTextView.setText("wrongs");
-            }
-        });
+        ImageRequest imageRequest = QRCode.GenerateQR(url, mImageView);
         queue.add(imageRequest);
-
-
+        //Set Internet Receive
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+        String url2 = "http://api.qrserver.com/v1/read-qr-code/?fileurl=http%3A%2F%2Fapi.qrserver.com%2Fv1%2Fcreate-qr-code%2F%3Fdata%3DHelloWorld";
+        JsonArrayRequest jsonRequest = QRCode.GetQRInfo(url2, mTextView);
+        queue.add(jsonRequest);
     }
 }
