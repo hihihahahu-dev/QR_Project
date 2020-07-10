ae3bac1c0d24a48946711c99bb52ad3475c471ab
mingyig3 <35508418+mingyig3@users.noreply.github.com>
2020-07-10 01:57:33 -0500

    Commit

--- app/src/main/java/com/example/qrproject/MainActivity.java
+++ app/src/main/java/com/example/qrproject/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.qrproject;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set Internet Request
+        final TextView mTextView = (TextView) findViewById(R.id.testing);
+        final ImageView mImageView = (ImageView) findViewById(R.id.imageView1);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100";
+        final ImageRequest imageRequest = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        // Display the first 500 characters of the response string.
+                        mImageView.setImageBitmap(response);
+                    }
+                }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                mTextView.setText("wrongs");
+            }
+        });
+        queue.add(imageRequest);
+
+
     }
 }
