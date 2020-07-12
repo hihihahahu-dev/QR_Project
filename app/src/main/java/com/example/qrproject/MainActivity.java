7ae8293bfc835bbb72de30cae9c2b9f5a295cd9d
mingyig3 <35508418+mingyig3@users.noreply.github.com>
2020-07-12 06:05:46 -0500

    Commit

--- app/src/main/java/com/example/qrproject/MainActivity.java
+++ app/src/main/java/com/example/qrproject/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.qrproject;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
@@ -11,14 +20,20 @@ import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    Uri Image_Uri;
+    ImageView testImageView;
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Set Internet Request
         final ImageView mImageView = (ImageView) findViewById(R.id.imageView1);
-        RequestQueue queue = Volley.newRequestQueue(this);
+        final RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100";
         ImageRequest imageRequest = QRCode.GenerateQR(url, mImageView);
         queue.add(imageRequest);
@@ -27,5 +42,54 @@ public class MainActivity extends AppCompatActivity {
         String url2 = "http://api.qrserver.com/v1/read-qr-code/?fileurl=http%3A%2F%2Fapi.qrserver.com%2Fv1%2Fcreate-qr-code%2F%3Fdata%3DHelloWorld";
         JsonArrayRequest jsonRequest = QRCode.GetQRInfo(url2, mTextView);
         queue.add(jsonRequest);
+        //Open Camera
+        final Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // get an image from the camera
+                testImageView = (ImageView) findViewById(R.id.imageView2);
+                openCamera();
+            }
+        });
+        //Take a picture
+        final Button button2 = findViewById(R.id.button_test);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Post image to API
+                final TextView mTextView = (TextView) findViewById(R.id.textView2);
+                String url3 = "http://api.qrserver.com/v1/read-qr-code/";
+                VolleyMultipartRequest multRequest = QRCode.GetQRPhoto(url3, mTextView, getFileDataFromDrawable(bitmap));
+                queue.add(multRequest);
+            }
+        });
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Image");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Testing");
+        Image_Uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent CameraInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        CameraInt.putExtra(MediaStore.EXTRA_OUTPUT, Image_Uri);
+        startActivityForResult(CameraInt, 1001);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Image_Uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            testImageView.setImageURI(Image_Uri);
+        }
     }
 }
--- app/src/main/java/com/example/qrproject/QRCode.java
+++ app/src/main/java/com/example/qrproject/QRCode.java
@@ -1,9 +1,12 @@
 package com.example.qrproject;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -14,7 +17,12 @@ import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRCode {
     public static ImageRequest GenerateQR(String url, final ImageView image) {
@@ -44,5 +52,33 @@ public class QRCode {
                 error.printStackTrace();
             }
         });
+    }
+    public static VolleyMultipartRequest GetQRPhoto(String url, final TextView text, final byte[] Imagery) {
+        return new VolleyMultipartRequest(Request.Method.POST, url,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONArray obj = new JSONArray(new String(response.data));
+                            text.setText(obj.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imaginary = System.currentTimeMillis();
+                params.put("file", new DataPart(imaginary + ".png", Imagery));
+                return params;
+            }
+        };
     }
 }
